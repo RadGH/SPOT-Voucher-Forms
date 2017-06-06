b47eddaa94067f670ba0ce6c091cdebfd31e7341
Radley <radleygh@gmail.com>
2017-06-06 00:26:47 -0700

    Beginning of the plugin

---
+++ spot-voucher-forms.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: SPOT Voucher Forms
+Description: Pet Spay and Neuter applications made through Gravity Forms can be managed by SPOT staff members, and a printable form can be downloaded.
+Version: 1.0.0
+Author: Radley Sustaire
+Author URI: http://radleysustaire.com/
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
